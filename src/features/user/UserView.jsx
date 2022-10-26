18b6233e9e21d0964427fbcf664b0e1f42908532
Joseph Mutua <Mutuaj793@gmail.com>
2022-10-27 00:35:10 +0300

    Fetching data

--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 export const UserView = () => {
-  return <div>List of Users</div>;
+  const user = useSelector((state) => state.user);
+ console.log("USER OBJECT",user)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  return (
+    <div>
+      <div>List of Users</div>
+      {user.loading && <div>Loading...</div>}
+      {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
 };
